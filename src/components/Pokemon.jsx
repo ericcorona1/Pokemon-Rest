77ddfa23e38d561f2b641a99a4ba4fc6cdff9899
ericcorona1 <ericcorona3@gmail.com>
2023-08-05 14:35:53 -0700

    pokedex entries

--- src/components/Pokemon.jsx
+++ src/components/Pokemon.jsx
@@ -3,20 +3,27 @@ import Description from "./Description";
 import PokeImg from "./PokeImg";
 
 const Pokemon = () => {
-    const randomId = () => {
-        return Math.floor(Math.random() * 1281)
-    }
+  const randomId = () => {
+    return Math.floor(Math.random() * 20);
+  };
 
-  fetch(`https://pokeapi.co/api/v2/pokemon/${randomId()}`).then((response) =>
-    response.json()
-  ).then(data => {
-    const {name, official_artwork, }
-  });
+  fetch(`https://pokeapi.co/api/v2/pokemon/${randomId()}`)
+    .then((response) => response.json())
+    .then((pokemonData) => {
+      const { name, official_artwork } = pokemonData;
+
+    });
+
+  fetch(`https://pokeapi.co/api/v2/pokemon-species/${randomId()}/`)
+    .then((response) => response.json())
+    .then((pokedexData) => {
+      const { flavor_text_entries } = pokedexData;
+    });
 
   return (
     <div>
-      <PokeImg pokeimg={pathToImage} alt={name} />
-      <Description name={name} description={description} />
+      <PokeImg pokeimg={official_artwork.front_default} alt={name} />
+      <Description name={name} description={flavor_text_entries} />
     </div>
   );
 };
