b06cb6e1e2d7fb212c30ea2a104ede29a57f6ed8
ericcorona1 <ericcorona3@gmail.com>
2023-08-06 00:27:01 -0700

    fetch name
    trying to figure out how to get name and render it

--- src/components/Pokemon.jsx
+++ src/components/Pokemon.jsx
@@ -1,29 +1,42 @@
 import React from "react";
 import Description from "./Description";
 import PokeImg from "./PokeImg";
+import { useState, useEffect } from "react";
 
 const Pokemon = () => {
-  const randomId = () => {
-    return Math.floor(Math.random() * 20);
-  };
+  const [randomId, setRandomId] = useState(() =>
+    Math.floor(Math.random() * 20)
+  );
+  const [pokeName, setPokeName] = useState("");
+  const [pokeImage, setPokeImage] = useState("");
 
-  fetch(`https://pokeapi.co/api/v2/pokemon/${randomId()}`)
-    .then((response) => response.json())
-    .then((pokemonData) => {
-      const { name, official_artwork } = pokemonData;
-
-    });
-
-  fetch(`https://pokeapi.co/api/v2/pokemon-species/${randomId()}/`)
-    .then((response) => response.json())
-    .then((pokedexData) => {
-      const { flavor_text_entries } = pokedexData;
-    });
+  useEffect(() => {
+    fetch(`https://pokeapi.co/api/v2/pokemon/${randomId}/`)
+      .then((response) => {
+        if (!response.ok) {
+          throw new Error("Failed to fetch data");
+        }
+        return response.json();
+      })
+      .then((pokemonData) => {
+        setPokeImage(pokemonData.official_artwork.front_default);
+        setPokeName(pokemonData.name);
+      })
+      .catch((error) => {
+        console.error(error);
+      });
+  }, []);
+  // fetch(`https://pokeapi.co/api/v2/pokemon-species/${randomId}/`)
+  //   .then((response) => response.json())
+  //   .then((pokedexData) => {
+  //     const { flavor_text_entries } = pokedexData;
+  //     console.log(flavor_text_entries);
+  //   });
 
   return (
     <div>
-      <PokeImg pokeimg={official_artwork.front_default} alt={name} />
-      <Description name={name} description={flavor_text_entries} />
+      <PokeImg pathToImage={pokeImage} name={pokeName} />
+      {/* <Description name={name} description={flavor_text_entries} /> */}
     </div>
   );
 };
