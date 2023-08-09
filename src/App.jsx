f514b875d38f5891ef5102b33a79d9d544f620b9
ericcorona1 <ericcorona3@gmail.com>
2023-08-08 18:31:54 -0700

    app render

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,51 @@
-import './App.css'
-import Pokemon from './components/Pokemon.jsx'
+import "./App.css";
+import React, { useEffect, useState } from "react";
+import RenderData from "./components/RenderData";
+import RandomBtn from "./components/RandomBtn";
+import Footer from "./components/Footer";
 
 function App() {
+  const [randomId, setRandomId] = useState(Math.floor(Math.random() * 1010));
+  console.log(randomId);
+  const [name, setName] = useState("");
+  const [image, setImage] = useState("");
+  const [description, setDescription] = useState("");
+
+  // Fetch Pokemon Data and set it
+  useEffect(() => {
+    fetch(`https://pokeapi.co/api/v2/pokemon/${randomId}/`)
+      .then((response) => response.json())
+      .then((data) => {
+        setImage(data.sprites.other["official-artwork"].front_default);
+        setName(data.name);
+      });
+    fetch(`https://pokeapi.co/api/v2/pokemon-species/${randomId}/`)
+      .then((response) => response.json())
+      .then((data) => {
+        const flavorText = data.flavor_text_entries;
+        const englishPokedexEntry = flavorText.find(
+          (entry) => entry.language.name === "en"
+        );
+        setDescription(
+          englishPokedexEntry
+            ? englishPokedexEntry.flavor_text
+            : `This Pokemon doesn't have an English Pokedex entry`
+        );
+      });
+  }, [randomId]);
+
+  const generateRandomId = () => {
+    const newRandomId = Math.floor(Math.random() * 1010);
+    setRandomId(newRandomId);
+  };
 
   return (
-    <>
-      <Pokemon />
-    </>
-  )
+    <div>
+      <RenderData name={name.toUpperCase()} image={image} description={description} />
+      <RandomBtn newRandomId={generateRandomId} />
+      <Footer />
+    </div>
+  );
 }
 
-
-export default App
+export default App;
