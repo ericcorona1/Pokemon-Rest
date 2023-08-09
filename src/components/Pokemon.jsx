07eae51c5560a9b54b9bea001139696b1233a39a
ericcorona1 <ericcorona3@gmail.com>
2023-08-08 18:34:34 -0700

    combined img and pokedex entry

--- src/components/Pokemon.jsx
+++ src/components/Pokemon.jsx
@@ -1,44 +1,13 @@
-import React from "react";
-import Description from "./Description";
-import PokeImg from "./PokeImg";
-import { useState, useEffect } from "react";
+import FetchImage from "./FetchImage";
+import FetchDescription from "./FetchDescription";
 
-const Pokemon = () => {
-  const [randomId, setRandomId] = useState(() =>
-    Math.floor(Math.random() * 20)
-  );
-  const [pokeName, setPokeName] = useState("");
-  const [pokeImage, setPokeImage] = useState("");
-
-  useEffect(() => {
-    fetch(`https://pokeapi.co/api/v2/pokemon/${randomId}/`)
-      .then((response) => {
-        if (!response.ok) {
-          throw new Error("Failed to fetch data");
-        }
-        return response.json();
-      })
-      .then((pokemonData) => {
-        setPokeImage(pokemonData.official_artwork.front_default);
-        setPokeName(pokemonData.name);
-      })
-      .catch((error) => {
-        console.error(error);
-      });
-  }, []);
-  // fetch(`https://pokeapi.co/api/v2/pokemon-species/${randomId}/`)
-  //   .then((response) => response.json())
-  //   .then((pokedexData) => {
-  //     const { flavor_text_entries } = pokedexData;
-  //     console.log(flavor_text_entries);
-  //   });
-
+const RenderData = ({ name, image, description }) => {
   return (
     <div>
-      <PokeImg pathToImage={pokeImage} name={pokeName} />
-      {/* <Description name={name} description={flavor_text_entries} /> */}
+      <FetchImage pathToImage={image} alt={name} />
+      <FetchDescription name={name} description={description} />
     </div>
   );
 };
 
-export default Pokemon;
+export default RenderData;
